2f7f4d4ae5b2dc464dd40d80bf3849480bab5cdd
Tobia Carozzi <carozzi@lofar.eu>
2013-02-07 08:37:06 +0000

    Added parsing of subband frequencies from parset file. Note that the formula for the center frequency of the subband is only approximate, and will need to be modified in future versions.

--- parseParset.py
+++ parseParset.py
@@ -20,6 +20,22 @@ def getRA(obsID):
 def getDEC(obsID):
     return getParam(obsID,"Observation.Beam[0].angle2","float")
 
+def getSubbandList(obsID):
+    sblstGaps=getParam(obsID,"Observation.subbandList","list")
+    sblst=[]
+    for sblstElem in sblstGaps :
+        sbStart,sbStop=sblstElem.split('..',2)
+        sblst.extend(range(int(sbStart),int(sbStop)+1))
+    return sblst
+
+def getSubbandFreqs(obsID):
+    sbwidth=getParam(obsID,"Observation.subbandWidth","float")
+    sblst=getSubbandList(obsID)
+    freqs=[]
+    for sbNr in sblst:
+      freqs.append(sbNr*sbwidth*1000.0) #in Hz
+    return freqs
+
 def getParam(obsID,paramName,dataType):
 
     basename=obsID
@@ -39,8 +55,15 @@ def getParam(obsID,paramName,dataType):
             elif dataType == 'epoch':
                valStrip=valStrFull[1:-1]
                paramValue=valStrip
+            elif dataType == 'float':
+               valStrip=valStrFull
+               paramValue=float(valStrip)
             else:
                paramValue=valStrFull
         line=f.readline()
     #print paramValue
     return paramValue
+
+if __name__ == "__main__":
+   sbf=getSubbandFreqs('L29053')
+   print sbf
